67b600844fc282640112fabf5441073019cba159
Abhi <abhinay@snsoft.my>
2020-04-28 15:34:19 +0800

    Add Android native module

--- android/src/main/java/com/baiachen/mta/RNMtaPackage.java
+++ android/src/main/java/com/baiachen/mta/RNMtaPackage.java
@@ -1,19 +1,24 @@
-package com.reactlibrary;
+package com.baiachen.mta;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.NativeModule;
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.uimanager.ViewManager;
-import com.facebook.react.bridge.JavaScriptModule;
-
-public class MtaPackage implements ReactPackage {
+public class RNMtaPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-        return Arrays.<NativeModule>asList(new MtaModule(reactContext));
+        return Arrays.<NativeModule>asList(new RNMtaModule(reactContext));
+    }
+
+    // Deprecated from RN 0.47
+    public List<Class<? extends JavaScriptModule>> createJSModules() {
+        return Collections.emptyList();
     }
 
     @Override
